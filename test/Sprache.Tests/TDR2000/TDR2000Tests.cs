ea77ff242e18f0b019fbd378b7e72945954a15c6
Serabass <serabass565@gmail.com>
2023-12-25 20:11:34 +0600

    upd

--- test/Sprache.Tests/TDR2000/TDR2000Tests.cs
+++ test/Sprache.Tests/TDR2000/TDR2000Tests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using SpracheBinary;
@@ -7,13 +9,24 @@ namespace SpracheBinary.Tests.TDR2000
 {
   public struct TBlock
   {
-    public string str;
-    public int probOffset;
-    public int probSize;
+    public string fileName;
+    public int offset;
+    public int size;
   }
 
-  public class DirParser
+  public struct PakEntry {
+    public string name;
+    public IEnumerable<byte> data;
+  }
+
+  public class DirParser : IDisposable
   {
+    private FileStream dirStream;
+    private FileStream pakStream;
+    private string path;
+
+    private PakEntry[] pakEntries;
+
     static readonly Parser<char> TChar = from ch in Parse.AnyByte
                                          from unk in Parse.Bytes(0x40, 0xC0)
                                          select (char)ch;
@@ -26,19 +39,53 @@ namespace SpracheBinary.Tests.TDR2000
                                                     from end in TLastChar
                                                     select new string(chars.ToArray().Concat(new[] { end }).ToArray());
 
-    public static readonly Parser<TBlock> TBlock = from str in TString
-                                                   from probOffset in Parse.Int32
-                                                   from probSize in Parse.Int32
+    public static readonly Parser<TBlock> TBlock = from fileName in TString
+                                                   from offset in Parse.Int32
+                                                   from size in Parse.Int32
                                                    select new TBlock
                                                    {
-                                                     str = str,
-                                                     probOffset = probOffset,
-                                                     probSize = probSize
+                                                     fileName = fileName,
+                                                     offset = offset,
+                                                     size = size
                                                    };
 
     public static readonly Parser<TBlock[]> TDocument = from blocks in TBlock.Many()
-                                                         select blocks.ToArray();
+                                                        select blocks.ToArray();
 
+    public DirParser(string path)
+    {
+      this.path = path;
+    }
+
+    private PakEntry ReadPakEntry(int offset, int size, string name)
+    {
+      var reader = from _ in Parse.Seek(offset, SeekOrigin.Begin)
+                   from bytes in Parse.AnyByte.Repeat(size)
+                   select new PakEntry
+                   {
+                     name = name,
+                     data = bytes
+                   };
+
+      return reader.Parse(pakStream);
+    }
+
+    public DirParser Init()
+    {
+      dirStream = File.OpenRead(path + ".dir");
+      pakStream = File.OpenRead(path + ".pak");
+
+      var blocks = TDocument.Parse(dirStream);
+      pakEntries = blocks.Select(b => ReadPakEntry(b.offset, b.size, b.fileName)).ToArray();
+
+      return this;
+    }
+
+    public void Dispose()
+    {
+      dirStream.Dispose();
+      pakStream.Dispose();
+    }
   }
 
   public class TDR2000Tests
@@ -46,9 +93,9 @@ namespace SpracheBinary.Tests.TDR2000
     [Fact]
     public void ReadDirTest()
     {
-      using var dir = File.OpenRead(@"C:\Program Files (x86)\Новый Диск\Кармагеддон. Колеса смерти\Assets\MovableObjects\MOVABLEOBJECTS.dir");
-      dir.Position = 0;
-      var doc = DirParser.TDocument.Parse(dir);
+      using DirParser p = new DirParser(@"C:\Program Files (x86)\Новый Диск\Кармагеддон. Колеса смерти\Assets\MovableObjects\MOVABLEOBJECTS")
+        .Init();
+
     }
   }
 }
